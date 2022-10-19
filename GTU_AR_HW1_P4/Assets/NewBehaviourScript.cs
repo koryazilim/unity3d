51bed9b6b55181f269ff92ab7f13aee391dc39c3
BurhanettinB <54434720+koryazilim@users.noreply.github.com>
2022-10-19 21:52:33 +0300

    project files, first upload
    All items was performed

---
+++ GTU_AR_HW1_P4/Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+  
+    public void loadScene1(){
+      UnityEngine.SceneManagement.SceneManager.LoadScene("Scene1");
+    }
+    public void loadScene2(){
+      UnityEngine.SceneManagement.SceneManager.LoadScene("Scene2");
+    }
+}
